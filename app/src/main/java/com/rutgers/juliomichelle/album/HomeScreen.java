e7c0af13332898c2c4926e01e95cbc51c3cfb214
julio <50313766+jughv@users.noreply.github.com>
2022-05-01 20:23:48 -0400

    Initial commit

---
+++ app/src/main/java/com/rutgers/juliomichelle/album/HomeScreen.java
@@ -0,0 +1,14 @@
+package com.rutgers.juliomichelle.album;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
